eaefbf9ff855871975006d60dd8318194e1e9b6b
Hussaan S <hussaan.s@gmail.com>
2021-08-30 11:01:37 +0500

    Add CoreDataStack. Add CDAppointment Entity. Add Appointment DataStore (#2)

---
+++ Appointments/Classes/CoreData/Entities/CDAppointment+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CDAppointment+CoreDataClass.swift
+//  Appointments
+//
+//  Created by Hussaan S on 27/08/2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CDAppointment)
+public class CDAppointment: NSManagedObject {
+
+}
