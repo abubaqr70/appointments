7a5e7695db68d7976800f7908e53060f1a10d093
Hussaan S <hussaan.s@gmail.com>
2021-09-20 11:02:38 +0500

    Add resource bundle. Update podSpecs

--- Appointments/Source/AppointmentsKit/Extensions/Bundle+Extension.swift
+++ Appointments/Source/AppointmentsKit/Extensions/Bundle+Extension.swift
@@ -6,4 +6,18 @@ extension Bundle {
     static var module: Bundle {
         return Bundle(for: AppDependencyContainer.self)
     }
+    
+    static var resource: Bundle {
+        
+        let bundle = Bundle.module
+        guard let url = bundle.url(forResource: "Appointments", withExtension: "bundle") else  {
+            return bundle
+        }
+        
+        guard let resourceBundle = Bundle(url: url) else {
+            return bundle
+        }
+        
+        return resourceBundle
+    }
 }
